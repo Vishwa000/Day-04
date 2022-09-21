59fcb5f99d964c0518763c4b58d91fccf9cce113
Vishwa S <113334237+Vishwa000@users.noreply.github.com>
2022-09-21 16:52:55 +0530

    Print country name, region, subregion,population

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ request.onload=function(){
     var result=JSON.parse(request.response);
     for(var i=0;i<result.length; i++){
     
-      console.log(result[i].flag);
+      console.log(result[i].name,result[i].region,result[i].subregion,result[i].population);
   
     }
   }
